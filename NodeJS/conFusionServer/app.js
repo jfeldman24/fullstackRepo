770030865a6e5fcf43f7d3cbcc5a8c5d6f3029ea
jfeldman24 <jfeldman@stevens.edu>
2019-02-17 14:04:56 -0500

    Basic Authentification

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -12,17 +12,15 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
-
 const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url, {
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useNewUrlParser: true
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useNewUrlParser: true
 });
 
 connect.then((db) => {
-  console.log('Connected to MongoDB Server');
+  	console.log('Connected to MongoDB Server');
 }, (err) => {console.log(err); });
 
 var app = express();
@@ -35,6 +33,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next) {
+	console.log(req.headers);
+	  
+	var authHeader = req.headers.authorization;
+	if(!authHeader) {
+		var err = new Error('Invalid authentification');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err);
+	} 
+	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+	var username = auth[0];
+	var password = auth[1];
+
+	if(username === 'admin' && password === 'password') {
+		next();
+	} else {
+		var err = new Error('Invalid authentification');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err);
+	}
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -45,18 +70,18 @@ app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
